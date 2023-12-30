d4cc2aa3b92059358dd33edec77dc99c0396bed3
sp <stefan.pranger@iaik.tugraz.at>
2023-12-30 13:21:14 +0100

    debugging

--- examples/shields/rl/callbacks.py
+++ examples/shields/rl/callbacks.py
@@ -27,19 +27,9 @@ class ShieldInfoCallback(DefaultCallbacks):
         pass
 
 class MyCallbacks(DefaultCallbacks):
-    def on_algorithm_init(self, algorithm: Algorithm, **kwargs):
-        file_writer = tf.summary.FileWriter(algorithm.logdir)
-        with file_writer.as_default():
-            tf.summary.text("first_text", "testing", step=0)
-        file_writer.flush()
-
     def on_episode_start(self, *, worker: RolloutWorker, base_env: BaseEnv, policies: Dict[PolicyID, Policy], episode, env_index, **kwargs) -> None:
-        file_writer = tf.summary.FileWriter(worker.io_context.log_dir)
-        with file_writer.as_default():
-            tf.summary.text("first_text_from_episode_start", "testing_in_episode", step=0)
         with open(f"{worker.io_context.log_dir}/testing.txt", "a") as file:
             file.write("first_text_from_episode_start\n")
-        file_writer.flush()
         # print(F"Epsiode started Environment: {base_env.get_sub_environments()}")
         env = base_env.get_sub_environments()[0]
         episode.user_data["count"] = 0
