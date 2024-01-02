023887f97e484d3c5dd4036e68e4171ecd4fc0ff
Thomas Knoll <thomas.knoll@student.tugraz.at>
2024-01-02 10:37:26 +0100

    fixed testall

--- testall.py
+++ testall.py
@@ -6,11 +6,12 @@ import subprocess
 abs_path = os.getcwd()
 
 slippery_configs=[f"{abs_path}/slippery_prob_075.yaml", 
-                  f"{abs_path}/slippery_prob_08.yaml",
+                  # f"{abs_path}/slippery_prob_08.yaml",
                   f"{abs_path}/slippery_prob_085.yaml",
-                  f"{abs_path}/slippery_prob_09.yaml",
+                  # f"{abs_path}/slippery_prob_09.yaml",
                   f"{abs_path}/slippery_prob_095.yaml",
-                  f"{abs_path}/slippery_prob_1.yaml"]
+                  # f"{abs_path}/slippery_prob_1.yaml"
+                  ]
 
 slippery_probs=[[0.125, 0.25, 0.75], # 0.75
                 #[0.1, 0.2, 0.8],     # 0.8
@@ -40,13 +41,13 @@ ENV="MiniGrid-LavaSlipperyS12-v2"
 for shield_value in shield_values:
   for sh_comp in comparison_type:
     for probs, config in prob_confs:
-      command = f"echo \"Running experiment with sh:{sh}, sh_value:{probs[2]}, sh_comp:{sh_comp}, probvalues:{probs}, config{config}\""    
+      command = f"echo \"Running experiment with shielding full, sh_value:{probs[2]}, sh_comp:{sh_comp}, probvalues:{probs}, config{config}\""    
       execute_command = f'./syncscript.sh {NUM_TIMESTEPS} {LOGDIR} {"70"} {ENV} full {sh_comp} {config} {probs[0]} {probs[1]} {probs[2]} {shield_value}'
       subprocess.call(execute_command, shell=True)#.decode("utf-8").split('\n')
   
 # loop for unshielded runs
 for probs, config in prob_confs:
-  command = f"echo \"Running experiment with sh:{sh}, sh_value:{probs[2]}, sh_comp:{sh_comp}, probvalues:{probs}, config{config}\""    
+  command = f"echo \"Running experiment with shielding none, sh_value:{probs[2]}, sh_comp:{sh_comp}, probvalues:{probs}, config{config}\""    
   execute_command = f'./syncscript.sh {NUM_TIMESTEPS} {LOGDIR} {"70"} {ENV} none {sh_comp} {config} {probs[0]} {probs[1]} {probs[2]} {shield_value}'
   subprocess.call(execute_command, shell=True)#.decode("utf-8").split('\n')
   
