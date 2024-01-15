d194537f56993cf560f5c22639688ff24d7edf02
sp <stefan.pranger@iaik.tugraz.at>
2024-01-15 14:07:18 +0100

    shortened expname

--- examples/shields/rl/utils.py
+++ examples/shields/rl/utils.py
@@ -134,7 +134,7 @@ class MiniGridShieldHandler(ShieldHandler):
         return self.__create_shield_dict()
 
 def create_log_dir(args):
-    return f"{args.log_dir}/sh_{args.shielding}-value_{args.shield_value}-comp_{args.shield_comparison}-env_{args.env}-conf_{args.prism_config}"
+    return f"{args.log_dir}/{args.env}_{args.shielding}_{args.shield_comparison}_{args.shield_value}"
 
 def test_name(args):
     return f"{args.expname}"
